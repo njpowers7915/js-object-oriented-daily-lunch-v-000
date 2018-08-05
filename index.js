1a23e3026f53fc07d6366c3dd0fd3f46d40d26ec
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 18:05:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0
 class Neighborhood {
-
+  constructor(name) {
+    this.id = ++neighborhoodId
+    this.name = name
+    store.drivers.push(this)
+  }
 }
 
 let customerId = 0
