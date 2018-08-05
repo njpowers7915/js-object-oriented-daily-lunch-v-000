1531f0d9400c5acf23551c24c9c72101180b3eb5
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 18:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighborhoodId = 0
-class Neighborhood {
-  constructor(name) {
-    this.id = ++neighborhoodId
-    this.name = name
-    store.neighborhoods.push(this)
+let deliveryId = 0
+class Delivery {
+  constructor(meal, neighborhood, customer) {
+    this.id = ++deliveryId
+    this.mealId = meal.id
+    this.neighborhoodId = neighborhood.id
+    this.customerId = customer.id
+    store.deliveries.push(this) 
   }
 }
+
 
 let customerId = 0
 class Customer {
@@ -30,13 +33,14 @@ class Meal {
   }
 }
 
-let deliveryId = 0
-class Delivery {
-  constructor(meal, neighborhood, customer) {
-    this.id = ++deliveryId
-    this.mealId = meal.id
-    this.neighborhoodId = neighborhood.id
-    this.customerId = customer.id
-    store.deliveries.push(this) 
+
+let neighborhoodId = 0
+class Neighborhood {
+  constructor(name) {
+    this.id = ++neighborhoodId
+    this.name = name
+    store.neighborhoods.push(this)
   }
 }
+
+
