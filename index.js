58d102373c509aa26a3ab6e98c28d0ce723ff50a
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 18:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0
+class Neighborhood {
+  
+}
+
+let customerId = 0
+class Customer {
+  
+}
+
+class Meal {
+
+}
+
+class Delivery {
+  
+}
