35bdd42cd48983a39ee3cfc7d33312f5bf026c5e
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 18:04:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0
 class Neighborhood {
-  
+
 }
 
 let customerId = 0
 class Customer {
-  
+
 }
 
+let mealId = 0
 class Meal {
 
 }
 
 class Delivery {
-  
+
 }
